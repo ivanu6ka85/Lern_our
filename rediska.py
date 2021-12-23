7d81ddf63a9c013fce13326d253839f0ac6d7fa7
ivanu6ka85 <91127308+ivanu6ka85@users.noreply.github.com>
2021-12-23 14:36:13 +0300

    Update rediska.py

--- rediska.py
+++ rediska.py
@@ -3,6 +3,6 @@ import redis
 
 red = redis.Redis(
     host='redis-15836.c238.us-central1-2.gce.cloud.redislabs.com',
-    port = '15836',
+    port = '15836', #кавычки как просили)))
     password = '7KOedC9scbFq6PMwsAWEWHig1Ndl4Vj3'
 )
