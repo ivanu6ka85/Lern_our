347559b7a66ba92ed97de29f8355ca48ed4e270d
ivanu6ka85 <91127308+ivanu6ka85@users.noreply.github.com>
2022-03-08 13:30:48 +0300

    Update practica_selenium.py

--- practica_selenium.py
+++ practica_selenium.py
@@ -45,24 +45,20 @@ def test_show_my_pets():
     finally:
         driver.quit()
 
-    def test_show_my_pets():
-        # Вводим email
-        pytest.driver.find_element_by_id('email').send_keys('mia_85@mail.ru')
-        # Вводим пароль
-        pytest.driver.find_element_by_id('pass').send_keys('qwerty')
-        # Нажимаем на кнопку входа в аккаунт
-        pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
-        pytest.driver.find_element_by_class_name('navbar-toggler-icon').click()
-        pytest.driver.find_element_by_xpath('//*[contains(text(),"Мои питомцы")]').click()
-        try:
-            explicit_waits = WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="all_my_pets"]')))  # явные ожидания
-            assert explicit_waits == '//*[@id="all_my_pets"]'
-            print('Есть совпадение')
-        except TimeoutException:
-            print('Нет совпадений')
-        finally:
-            driver.quit()
+        images = pytest.driver.find_elements_by_xpath('//*[id="all_my_pets"]//img')
+        names = pytest.driver.find_elements_by_xpath('//*[@id="all_my_pets"]//body/tr/td[0]')
+        descriptions = pytest.driver.find_elements_by_xpath('//*[@id="all_my_pets"]/table/tbody')
+
+        for i in range(len(names)):
+            assert images[i].get_attribute('scr') != ''
+            assert names[i].text != ''
+            assert descriptions[i].text != ''
+            assert ',' in descriptions[i].text
+            parts = descriptions[i].text.split(", ")
+            assert len(parts[0]) > 0
+            assert len(parts[1]) > 0
+
+        driver.quit()
 
 animals_names = ['Piter', 'Leo', 'Егонет', 'Симба', 'Джо']
 unique = []
